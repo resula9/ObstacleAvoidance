07c9553916440a9c83c85a95a3b44cd5ad575849
oguz <koseo16@itu.edu.tr>
2018-02-03 16:17:48 +0300

    still json

--- freshstart.py
+++ freshstart.py
@@ -1,51 +1,47 @@
-# Introduction to Obstacle Avoidance
+# Fresh Start to Obstacle Avoidance
 import math
 import numpy as np
+import json
 
-file_points = open("mission.waypoints", "r")
-lines_points = np.asarray(file_points.read().split('\n'))
-point_number = len(lines_points) - 1
-points = np.zeros((point_number, 3))  # point number; lat, long, alt
-#
-home_point = [39.589000, -74.9507, 1]
+'''
+Drone-kit ten aracin konum bilgisi alinarak vehicle_point degiskenine atanacak
+
+'''
+
+
+class Mission(object):
+
+    def __init__(self, json_content):
+        data = json.loads(json_content)
+        for key, value in data.items():
+            self.__dict__[key] = value
+
+
+class Obstacle(object):
+
+    def __init__(self, json_content):
+        data = json.loads(json_content)
+        for key, value in data.items():
+            self.__dict__[key] = value
+
+
 #
 temp_point_number = 10
 temp_points = np.zeros((temp_point_number, 3))  # temp_point number; lat, long, alt
-#
-file_obstacles = open("obstacles.poly", "r")
-lines_obstacles = np.asarray(file_obstacles.read().split('\n'))
-obstacle_number = len(lines_obstacles) - 1
-obstacles = np.zeros((obstacle_number, 5))  # obstacle number; lat, long, alt, radius, speed
 #
 global trouble_points
 trouble_points = np.zeros((temp_point_number, 5))  # temp_point number; lat, long, alt, control, obstacle number
 #
 earth_radius = 6371000  # earth`s radius in meter
-
-
-def get_points():
-    for i in range(0, point_number):
-        values = np.asarray(lines_points[i + 1].split('\t'))
-        points[i, 0] = values[8]
-        points[i, 1] = values[9]
-        points[i, 2] = values[10]
-    print(points)
-    return points
-
-
-def get_obstacles():
-    for i in range(0, obstacle_number):
-        values = np.asarray(lines_obstacles[i + 1].split(' '))
-        print(values)
-        obstacles[i, 0] = values[0]
-        obstacles[i, 1] = values[1]
-        obstacles[i, 2] = values[2]
-        obstacles[i, 3] = values[3]
-        #######
-        obstacles[i, 4] = values[4]
-        #######
-    print(obstacles)
-    return obstacles
+#
+mission_file = open("missions.json", "r")
+missiondata = mission_file.read()
+mission = Mission(missiondata)
+#
+obstacle_file = open("obstacles.json", "r")
+obstacledata = obstacle_file.read()
+obstacle = Obstacle(obstacledata)
+#
 
 
 def get_temp_points(vehicle_position, reaching_point):
@@ -64,15 +60,16 @@ def get_distance(first_point, second_point):
     dlat = lat2 - lat1  # latitute difference in radian
     dlong = lng2 - lng1  # longitute difference in radian
 
-    a = math.pow(math.sin(dlat / 2), 2) + math.cos(lat1) * math.cos(lat2) * math.pow(math.sin(dlong / 2), 2)  # Haversine Formula
+    a = math.pow(math.sin(dlat / 2), 2) + math.cos(lat1) * math.cos(lat2) \
+        * math.pow(math.sin(dlong / 2), 2)  # Haversine Formula
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))  # Haversine Formula
     distance = earth_radius * c
     return distance
 
 
-def get_intersections(temp_points, obstacles):
+def get_intersections(temp_points, obstacle):
     for i in range(0, temp_point_number):
-        for j in range(0, obstacle_number):
+        for j in range(0, len(obstacle.stationary_obstacles) + len(obstacle.moving_obstacles)):
             if get_distance(temp_points[i], obstacles[j]) < obstacles[j, 2] * 1.2:
                 global trouble_points
                 trouble_points[i, 0] = temp_points[i, 0]
@@ -80,7 +77,3 @@ def get_intersections(temp_points, obstacles):
                 trouble_points[i, 2] = temp_points[i, 2]
                 trouble_points[i, 3] = 1
                 trouble_points[i, 4] = j
-
-#  temp point sayisini aradaki mesafeye gore belirlemek lazim
-#  temp waypoint konumalarini aracin  hizina ve gore tahmini yerlere atamak
-#  ustteki olayi obstacle lar icin de yapmak
--- reading_obtsacles.py
+++ reading_obtsacles.py
@@ -1,21 +1,32 @@
 import json
 
 
+class Mission(object):
+
+    def __init__(self, json_content):
+        missiondata = json.loads(json_content)
+        for key, value in missiondata.items():
+            self.__dict__[key] = value
+
+
 class Obstacle(object):
 
-    def __init__(self, moving_obstacles, stationary_obstacles):
-        self.moving_obstacles = moving_obstacles
-        self.stationary_obstacles = stationary_obstacles
+    def __init__(self, json_content):
+        obstacledata = json.loads(json_content)
+        for key, value in obstacledata.items():
+            self.__dict__[key] = value
+
+        self.__dict__('stationary_obstacles')[i].get('altitude') =
 
 
-def object_decoder(obj):
-    return Obstacle(obj['moving_obstacles'], obj['stationary_obstacles'])
+file1 = open("obstacles.json", "r")
+data = file1.read()
+data = str(data).replace('altitude_msl', 'altitude')
+data = str(data).replace('sphere_radius', 'radius')
+data = str(data).replace('cylinder_height', 'altitude')
+data = str(data).replace('cylinder_radius', 'radius')
+
+obstacle = Obstacle(data)
+print obstacle.moving_obstacles[1].get('altitude')
 
 
-file = open("jsonobstacle.json", "r")
-data = file.read()
-
-x = json.loads(data, object_hook=object_decoder)
-
-
-print(x.moving_obstacles)
